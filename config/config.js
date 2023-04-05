2ef54f160d247963eeb71740d1f0d2a0205470cf
rtacode <123898796+rtacode@users.noreply.github.com>
2023-04-05 18:48:06 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,5 @@
-const path = require('path')
-module.exports = {
-	src: path.resolve(__dirname, '../src'),
-	build: path.resolve(__dirname, '../dist'),
-	public: path.resolve(__dirname, '../src'),
-	pages: ['index', 'fontlist', 'units', 'adblock', '404']
-}
+[
+  {  
+      "tanggalpuasa":"April 2, 2023"
+  }
+]
