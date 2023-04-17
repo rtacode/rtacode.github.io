3b7872a5f3ebe6de89fd503243621af5966d9c1b
rtacode <123898796+rtacode@users.noreply.github.com>
2023-04-17 11:05:31 +0700

    Update

--- link/my-link.js
+++ link/my-link.js
@@ -1,7 +1,31 @@
+function DwApkFakecez() {
+  window.open("https://khaddavi.net/ip9u", "_blank");
+}
+
 function GetLinkKey() {
-  window.open("https://khaddavi.net/xGseXL1", "_self");
+  window.open("https://khaddavi.net/xGseXL1", "_blank");
 }
 
 function GetVerif() {
-  window.open("https://fakecez.com/random2/1ashortlink331.php", "_self");
+  window.open("https://fakecez.com/random2/1ashortlink771.php", "_blank");
+}
+
+function MyWeb() {
+  location.href = "https://rtacode.github.io/";
+}
+
+function Saweria() {
+  window.open("https://saweria.co/rizkikotet", "_blank");
+}
+
+function MyTele() {
+  location.href = "https://t.me/rizki_kotet";
+}
+
+function MyFb() {
+  location.href = "https://www.facebook.com/rtacodeteam";
+}
+
+function MyIg() {
+  location.href = "https://www.instagram.com/rizki_kotet";
 }
